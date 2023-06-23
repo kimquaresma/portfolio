4ca5ff5325001ab513d2db95ab35cb79a692c423
kimquaresma <tkdeo9090@gmail.com>
2023-06-24 02:18:13 +0900

    JS-Home

--- src/main.js
+++ src/main.js
@@ -4,11 +4,18 @@ const header = document.querySelector('.header');
 // console.log(headerRect);
 // const headerHeight = headerRect.height;
 // 위 세문장을 줄인게 아래 문장
-const headerHeight = header.getBoundingClientRect().height;
+const headerHeight = header.offsetHeight;
 document.addEventListener('scroll', () => {
     if(window.scrollY > headerHeight) {
         headear.classList.add('header--dark');
     } else {
         header.classList.remove('header--dark');
     }
-})
+});
+
+// Home 섹션을 아래로 스크롤시 투명하게 처리함
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', ()=> {
+    home.style.opacity = 1 - window.scrollY / homeHeight;
+});
